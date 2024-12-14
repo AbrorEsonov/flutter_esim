860be33d2c2bc4d4e669a5073795cae042997f48
abroresonovmacpro <AEsonov@beeline.uz>
2024-12-14 16:19:46 +0500

    chore(model):  -Add waiting success event

--- lib/flutter_esim.dart
+++ lib/flutter_esim.dart
@@ -1,7 +1,8 @@
+import 'dart:async';
+
 import 'flutter_esim_platform_interface.dart';
 
 class FlutterEsim {
-
   /// Check support eSIM.
   Future<bool> isSupportESim(List<String>? newer) async {
     return FlutterEsimPlatform.instance.isSupportESim(newer);
@@ -13,4 +14,12 @@ class FlutterEsim {
   }
 
   Stream<EsimInstallResponse?> get onEvent => FlutterEsimPlatform.instance.onEvent;
+
+  Future<EsimInstallResponse?> waitForSuccessEvent({Duration timeout = const Duration(seconds: 10)}) async {
+    try {
+      return FlutterEsimPlatform.instance.onEvent.where((event) => event?.event == "1").timeout(timeout).first;
+    } on TimeoutException {
+      return null;
+    }
+  }
 }
